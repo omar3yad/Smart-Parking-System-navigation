f6bd79a1cc8462612ec876b9bc1f8deb6b94647d
sama-sameh <114023728+sama-sameh@users.noreply.github.com>
2026-03-16 07:53:36 +0200

    edit sending data for backend

--- smart-parking-system-main/gates/APIClient.py
+++ smart-parking-system-main/gates/APIClient.py
@@ -1,22 +1,40 @@
+import cv2
 import requests
 
 class APIClient:
+
     def __init__(self, base_url):
         self.base_url = base_url
-    def send_to_backend(self, image_path, plate_text):
+        self.HEADERS = {'X-Camera-Key': 'my_ultra_secure_camera_token_2026'}
 
-        with open(image_path, "rb") as img:
-            files = {
-                "entry_image": img
-            }
-            data = {
-                "license_plate": plate_text,
-                "camera_id": 1
-            }
+    def send_to_backend(self, image, plate_text):
 
-            response = requests.post(self.base_url, files=files, data=data)
+        # convert frame to jpeg
+        if image is None:
+            print("No image to send")
+            return
+        _, img_encoded = cv2.imencode('.jpg', image)
+        if self.base_url.endswith("/entry/"):
+          files = {
+            "entry_image": ("image.jpg", img_encoded.tobytes(), "image/jpeg")
+          }
+        elif self.base_url.endswith("/exit/"):
+          files = {
+            "exit_image": ("image.jpg", img_encoded.tobytes(), "image/jpeg")
+          }
+        data = {
+            "license_plate": plate_text,
+            "camera_id": 1
+        }
 
-        if response.status_code == 200:
+        response = requests.post(
+            self.base_url,
+            files=files,
+            data=data,
+            headers=self.HEADERS
+        )
+
+        if response.status_code == 201 or response.status_code == 200:
             print("✅ Sent to backend successfully")
         else:
             print("❌ Backend error:", response.text)
